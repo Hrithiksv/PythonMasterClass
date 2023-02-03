287d85b620acbcd34421dfcb78baf5aa1e89eb04
Hrithik H V <hrithiksv98@gmail.com>
2023-02-03 11:20:54 +0530

    Feb_3rd

--- venv1/DSA/Arrays.py
+++ venv1/DSA/Arrays.py
@@ -2,7 +2,7 @@ import array as arr
 
 
 def Method1():
-    """Declare a string"""
+    """Declare a array"""
     ary1 = arr.array("i", [1, 3, 4, 5, 5, 6])
     # ary1.byteswap()
 
@@ -15,7 +15,7 @@ def Method1():
 
 
 def Method2():
-    """Reverse a string by swapping elements """
+    """Reverse a array by swapping elements """
     ary1 = arr.array("i", [1, 3, 4, 5, 5, 6])
     start = 0
     end = len(ary1)-1
@@ -30,7 +30,7 @@ def Method2():
 
 
 def Method3():
-    """Reverse string by List slicing"""
+    """Reverse array by List slicing"""
     ary1 = arr.array("i", [1, 3, 4, 5, 5])
     print(type(ary1))
     A = list(ary1)
@@ -52,14 +52,20 @@ def LeftRotate(x):
 
 # LeftRotate(8)
 
-def SecondLargest(ar: list):
-    ar = set(ar)
-    lst = list(ar)
-    return lst[-3]
+def SecondLargest(arr: list):
+    arr = set(arr)
+    if len(arr) <= 1:
+        return -1
+
+    # print(ar)
+    lst = list(arr)
+    lst.sort()
+    # print(lst)
+    return lst[-2]
 
 
-X = [2, 3, 14, 15, 16, 7, 8, 15, 16]
-#res = SecondLargest(X)
+# X = [2, 3, 14, 15, 16, 7, 8, 15, 16]
+# res = SecondLargest(X)
 # print(res)
 
 
@@ -72,9 +78,45 @@ def SecondLargest2():
     print(temp)
     temp2 = 0
     for i in range(len(Y)):
-        if temp2 < Y[i] and Y[i] != temp:  # IMP Y[i]!= temp
+        if temp2 < Y[i] and Y[i] != temp:
             temp2 = Y[i]
     print(temp2)
 
 
 # SecondLargest2()
+def ValueEqualToIndex(arr):
+    ary = []
+    for index, item in enumerate(arr, 1):
+        if index == item:
+            ary.append(index)
+    return ary
+
+
+# X = [3, 2, 3, 9, 9, 9]
+# val = ValueEqualToIndex(X)
+
+def MoveNonZeros():
+    ary = [2, 4, 0, 0, 1, 3, 5, 0]
+    nonzero = [x for x in ary if x != 0]
+    zero = [y for y in ary if y == 0]
+    return nonzero+zero
+
+
+# VAL = MoveNonZeros()
+# print(VAL)
+def Swapsubarrofksize(ary, N, K):
+    i = 0
+    while (i < N):
+        if (i+K < N):
+            ary[i:i+K] = reversed(ary[i:i+K])
+            i += K
+        else:
+            # reversed function used to reverse any part of the array.
+            ary[i:] = reversed(ary[i:])
+        # updating index from i to i+k.
+            i += K
+
+
+# ary = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9]
+# Swapsubarrofksize(ary, len(ary), 3)
+# print(ary)
--- venv1/PythonBasics/check.py
+++ venv1/PythonBasics/check.py
@@ -1,6 +1,3 @@
-#      0  1    2    3  4   5  6   7   8
-arr = [8, 22, 256, 53, 47, 4, 6, 98, 45]
-#      -9 -8  -7   -6  -5  -4 -3 -2  -1
-print(len(arr))
-for i in range(len(arr)):
-    print(i)
+a = 5
+b = 2
+print(b % a)
